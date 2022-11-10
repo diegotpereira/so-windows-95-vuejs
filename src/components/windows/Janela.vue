5ae4e5ad90248f7a46c3104942424aeeadc5ad92
diegotpereira <diegoteixeirapereira@hotmail.com>
2022-11-10 15:39:27 -0300

    adicionado arquivos de imagem.

--- src/components/windows/Janela.vue
+++ src/components/windows/Janela.vue
@@ -2,6 +2,7 @@
     <div class="janela">
         <div class="menu-bar">
             <div class="titulo">
+                <!-- <span class="icone" :style="{backgroundImage: 'url(' + require('@/assets/icon/' + arquivoIcone + '.png') + ')', }"></span> -->
 
             </div>
             <div class="acoes">
@@ -17,12 +18,36 @@
             </div>
         </div>
         <div class="carregar-programa">
+            <component :is="carregarPrograma"
+                       :arquivoNome="arquivoNome"
+                       :arquivoIcone="arquivoIcone"
+                       :arquivoTipo="arquivoTipo"
+                       :programasAberto="programasAberto"
+                       @abrirPrograma="abrirPrograma">
+
+            </component>
 
         </div>
     </div>
 </template>
 <script>
 export default {
-    name: 'paginaJanela'
+    name: 'paginaJanela',
+    data() {
+        return {
+            carregarPrograma: this.arquivoTipo
+        }
+    },
+    props: {
+        arquivoNome: String,
+        arquivoIcone: String,
+        arquivoTipo: String,
+        programasAberto: Array
+    },
+    methods: {
+        abrirPrograma(arquivoIcone) {
+            this.$emit("abrirPrograma", arquivoIcone);
+        }
+    }
 }
 </script>
